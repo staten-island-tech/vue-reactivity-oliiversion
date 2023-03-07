f941a650a37770abaf1650e1511dc556b6ece68a
oliiversion <olivxrkei101@gmail.com>
2023-03-07 13:16:50 -0500

    wow

--- vue-reactivity/src/App.vue
+++ vue-reactivity/src/App.vue
@@ -1,11 +1,15 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+//
+<script setup></script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -83,3 +87,5 @@ nav a:first-of-type {
   }
 }
 </style>
+
+//*
