5e7cec3587010e016199468faf80896d55b1851e
oliiversion <olivxrkei101@gmail.com>
2023-03-08 13:13:29 -0500

    wow

--- vue-reactivity/src/views/HomeView.vue
+++ vue-reactivity/src/views/HomeView.vue
@@ -1,9 +1,23 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from "../components/TheWelcome.vue";
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="Home">
+    <h1>Hello</h1>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
+
+<style>
+h1 {
+  color: rgb(255, 0, 0);
+  height: 50px;
+}
+</style>
