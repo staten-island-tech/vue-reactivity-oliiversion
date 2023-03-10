a1b91eb7874bba784d68b0aee173deacbcb34950
oliiversion <olivxrkei101@gmail.com>
2023-03-10 13:13:17 -0500

    wow

--- vue-reactivity/src/views/AboutView.vue
+++ vue-reactivity/src/views/AboutView.vue
@@ -1,8 +1,22 @@
 <template>
   <div class="about">
     <h1>hola amigos</h1>
+    <input type="text" placeholder="edit me" v-model="message" />
+    <p>{{ message }}</p>
   </div>
 </template>
+
+<script>
+export default {
+  name: "about",
+  components: {},
+  data() {
+    return {
+      message: "",
+    };
+  },
+};
+</script>
 
 <style>
 @media (min-width: 1024px) {
