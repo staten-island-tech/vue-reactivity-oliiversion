210fe93e2470570db5e86f1d67be21d313ef907c
oliiversion <olivxrkei101@gmail.com>
2023-03-10 13:05:07 -0500

    wow

--- vue-reactivity/src/views/HomeView.vue
+++ vue-reactivity/src/views/HomeView.vue
@@ -4,7 +4,13 @@ import TheWelcome from "../components/TheWelcome.vue";
 
 <template>
   <div class="Home">
-    <h1>{{ sillyquote }}</h1>
+    <h1 v-if="silly">{{ sillyquote }}</h1>
+    <h1 v-else>not silly.</h1>
+    <ul>
+      <li v-for="thing in sillythings" :key="thing">{{ thing }}</li>
+    </ul>
+    <button v-on:click="authState" v-if="inside">Inside!</button>
+    <button @click="authState" v-else>Outside!</button>
   </div>
 </template>
 
@@ -15,7 +21,19 @@ export default {
   data() {
     return {
       sillyquote: "them silly little kids",
+      silly: true,
+      sillythings: ["loser", "winner", "blue"],
+      inside: false,
     };
+  },
+  methods: {
+    authState: function () {
+      if (this.inside === false) {
+        this.inside = true;
+      } else {
+        this.inside = false;
+      }
+    },
   },
 };
 </script>
